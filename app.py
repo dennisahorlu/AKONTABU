067af42db3b686f33980fc338e9c2ad5b8c9224a
dennisahorlu <ahorlud@gmail.com>
2025-03-17 20:23:08 +0000

    Modification to the flask & other files

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
-app=Flask(__name__,static_folder='Static')
+app=Flask(__name__,static_folder='Static', template_folder='templates')
 
 @app.route('/')
 def csshtml():
     return render_template('index.html')
 
-app.run(host='0.0.0.0', port=80)
+@app.route('/signup')
+def signup():
+    return render_template('Signup.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
